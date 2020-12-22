6c4b4938066c2c43189f4ae3fde9bd75dfb81bf7
Connor Todd <connortodd0304@gmail.com>
2020-12-22 13:12:10 +0000

    Challenge 9

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { CartContext } from "../contexts/CartContext";
+import Product from "./Product";
 
 function CartList(props) {
+  const { cartProducts } = React.useContext(CartContext);
   return (
     <div>
       <h1>Cart List</h1>
+      {cartProducts.map((cartProduct) => (
+        <Product {...cartProduct} />
+      ))}
     </div>
   );
 }
