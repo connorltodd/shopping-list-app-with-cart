53723d67792928ac5f3819660143c4784b19c263
Connor Todd <connortodd0304@gmail.com>
2020-12-22 12:43:27 +0000

    Challenge 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ProductList from "./components/ProductList";
+import CartContextProvider from "./contexts/CartContext";
 
 function App() {
   return (
     <div className="App">
-      <ProductList />
+      <CartContextProvider>
+        <ProductList />
+      </CartContextProvider>
     </div>
   );
 }
